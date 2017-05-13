b3626f47acff040493d41fe6b7e8f30032d17dce
Mafalda Garcia <mafajotta@gmail.com>
2017-05-13 20:46:48 +0200

    Converter is woriking

--- txt_to_csv.py
+++ txt_to_csv.py
@@ -14,18 +14,31 @@ import csv
 import time
 start_time = time.time()
 
-#file paths
-movies_path = './movies.txt'
-reviews_path = './reviews.csv'
 
-movies = open(input_path, 'r')
-reviews = open(output_file_path, 'w')
+def convert(string):
+
+    i= string.find('/')
+    a = float(string[:i])
+    b = float(string[i+1:])
+    if b!=0:
+        result = a/b
+    else:
+        result = 0
+        
+    return result
+
+#file paths
+movies_path = './data/movies.txt'
+reviews_path = './data/reviews.csv'
+
+movies = open(movies_path, 'rb')
+reviews = open(reviews_path, 'wb')
 writer = csv.writer(reviews)
 
 columns = ['productid','userid','score']
 
 #writes header of 'reviews.csv'
-write.writerow(columns)
+writer.writerow(columns)
 
 dict={}
 for line in movies:
@@ -33,28 +46,33 @@ for line in movies:
     
     #there is an empty line after each review entry
     if not line:
-        #----->filter with helpfulness!
-        dict={}
+        helpfulness = dict.get('helpfulness')
+        if helpfulness >= 0.5:
+            writer.writerow([dict.get(column) for column in columns])
+            dict={}
+        else:
+            dict={}
     else:
+        #the key should have the same name as the columns
         end = line.find(':')
-        key = line[:end]
-        start = key.find('/')
-        key = line[start:]
+        start = line.find('/')
+        key = line[start+1:end]
         key = key.lower().strip()
         
+        string_value = line[end+1:]
+        
         if key == 'helpfulness':
-            #-------->convert from string to float
-            
+            value = convert(string_value)
         else:
-            value = line[end+1:]
-            value = value.strip()
+            value = string_value.strip()
         
         dict[key] = value
+        
 if dict:
     writer.writerow([dict.get(column) for column in columns])
 
 
-
+print('File converted successfully!')
 print("--- Time: %s seconds ---" % (time.time() - start_time))
 
 #closes files
