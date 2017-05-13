dc125b58fd8316cd26059e3757a999838fb36169
Mafalda Garcia <mafajotta@gmail.com>
2017-05-13 20:47:27 +0200

    Added similarity function

--- recommender.py
+++ recommender.py
@@ -12,19 +12,27 @@ import sys
 import math
 import time
 import pandas as pd
+import numpy as np
 
 
 start_time = time.time()
 
 #calculates similarity between two products
 def sim(product1,product2):
+
+    product1a=product1-product1.mean()
+    product2a=product2-product2.mean() 
     
+    num= np.sum(product1a*product2a)
+    den= np.sqrt(np.sum(product1a**2))*np.sqrt(np.sum(product2a**2))
+    
+    result =num/den
     
     return result
     
 ###########################
 #predicts rating the user would give to a certain movie
-def predict():
+def predict(user_ratings,similarity,user_products):
 
 
 
@@ -36,6 +44,14 @@ def main():
     
     reviews_path = './reviews.csv'
     
+    data = pd.read_csv(reviews_path)
+    
+    #saves all the reviewed movies to a series
+    all_products = data.productid
+    
+    #saves the movies reviewed by the user and their ratings
+    user_products = data[data.userid == user].productid
+    user_ratings = data[data.userid == user].score
     
     
     
